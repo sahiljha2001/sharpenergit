88d9adc40b980429e60db3d8f5f12d1f46caee7b
sahiljha2001 <sahiljha1001@google.com>
2023-03-27 15:10:40 +0530

    queryselector all

--- gitjs/sahil.js
+++ gitjs/sahil.js
@@ -8,30 +8,68 @@
 // console.log(document.links);
 // console.log(document.images);
 // console.log(document.domain);
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
 // headerTitle.textContent='hello';
-headerTitle.innerText='sahil';
-headerTitle.innerHTML='<h3>sahil</h3>';
-headerTitle.style.color='red';
-headerTitle.style.borderBottom ='solid 3px #000';
+// headerTitle.innerText='sahil';
+// headerTitle.innerHTML='<h3>sahil</h3>';
+// headerTitle.style.color='red';
+// headerTitle.style.borderBottom ='solid 3px #000';
 // GET ELEMENTS BY CLASS NAME
-var item = document.getElementsByClassName('list-group-item');
-console.log(item);
-console.log(item[2]);
-item[1].textContent='sahil';
-item[2].style.fontweight='bold';
-item[1].style.backgroundcolor='red';
- for(let i=0;i<item.length;i++){
-    item[i].style.backgroundcolor='green';
- }
+// var item = document.getElementsByClassName('list-group-item');
+// console.log(item);
+// console.log(item[2]);
+// item[1].textContent='sahil';
+// item[2].style.fontweight='bold';
+// item[1].style.backgroundcolor='red';
+//  for(let i=0;i<item.length;i++){
+//     item[i].style.backgroundcolor='green';
+//  }
  //GET ELEMNET BY TAG NAME;
- var li = document.getElementsByTagName('li');
- console.log(li);
- console.log(li[2]);
-li[1].textContent='sahil';
-li[2].style.fontweight='bold';
-li[1].style.backgroundcolor='red';
- for(let i=0;i<li.length;i++){
-    li[i].style.backgroundcolor='green';
- }
+//  var li = document.getElementsByTagName('li');
+//  console.log(li);
+//  console.log(li[2]);
+// li[1].textContent='sahil';
+// li[2].style.fontweight='bold';
+// li[1].style.backgroundcolor='red';
+//  for(let i=0;i<li.length;i++){
+//     li[i].style.backgroundcolor='green';
+//  }
+//GET ELEMENTS BY QUERYSELECTORS
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+// var input = document.querySelector('input');
+// input.value ='SEND';
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+// var firstItem = document.querySelector('.list-group-item:nth-child(2)');
+// firstItem.style.backgroundColor='green';
+// var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+
+// secondItem.style.display='none';
+//GET ELEMENTS BY QUERYSELECTORS
+
+
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent ='hello';
+// var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++)
+// {
+// odd[i].style.backgroundColor='green';
+// even[i].style.color='green';
+// }
+// even[1].style.color='black';
+
+
+
+
+
+
+
+
+
+
